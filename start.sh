4061fc8206c764a0abfbd31d30cc998412267e4e
jun.fu <fujunchn@qq.com>
2020-09-06 15:05:20 +0800

    feat: 添加sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+###
+ # @Author: jun.fu
+ # @LastEditors: jun.fu
+ # @Description: docker自动编译部署
+ # @Date: 2020-09-06 15:04:14
+ # @LastEditTime: 2020-09-06 15:04:45
+ # @FilePath: \test-vue\start.sh
+### 
+git pull
+
+yarn --registry=https://registry.npm.taobao.org/ && yarn build
+
+#删除容器
+docker rm -f demo1 &> /dev/null
+
+#启动容器
+docker run -d --restart=on-failure:5\
+    -p 8080:80 \
+    -v $PWD/dist:/usr/share/nginx/html \
+    --name demo1 nginx
