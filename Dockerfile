ff2a8644f5ba4a67e7e3aa7074b7117033ce8e5b
fujun <fujunchn@qq.com>
2020-09-25 00:14:11 +0800

    修改dockerfile和sh

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM nginx
 
 MAINTAINER signdigit1<fujunchn@qq.com>
 
-EXPOSE 8080
+EXPOSE 8080 80
 
 COPY dist/  /usr/share/nginx/html/
--- start.sh
+++ start.sh
@@ -5,12 +5,13 @@ git pull origin master
 yarn --registry=https://registry.npm.taobao.org/ && yarn build
 
 #删除容器
-docker rm -f demo1
+docker rm -f demo1 &> /dev/null
 
-docker build -t demo1 --no-cache
+echo $PWD
+
+docker build -t demo1 --no-cache .
 
 #启动容器
 docker run -d --restart=on-failure:5\
     -p 8080:80 \
-    # -v $PWD/dist:/usr/share/nginx/html \
     --name demo1 demo1
